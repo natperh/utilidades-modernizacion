ecec4b6c3916fbef8892ce98670b6cc9884ef2e3
natperh <115506506+natperh@users.noreply.github.com>
2026-03-30 20:19:34 -0600

    Update transformar.py

--- transformar.py
+++ transformar.py
@@ -50,7 +50,7 @@ def ejecutar_modernizacion():
         try:
             response = bedrock.invoke_model(
                 body=body, 
-                modelId="anthropic.claude-3-5-sonnet-20240620-v1:0"
+                modelId="anthropic.claude-3-5-sonnet-20241022-v2:0"
             )
             
             resultado = json.loads(response.get('body').read())
